f25da9ab740e3de82d982a60ed3457df90af82ab
chasepvazquez <Chasepvazquez@icloud.com>
2025-03-06 22:20:30 -0600

    inital commit

---
+++ CollegePads/App/CollegePadsApp.swift
@@ -0,0 +1,32 @@
+//
+//  CollegePadsApp.swift
+//  CollegePads
+//
+//  Created by Chase Vazquez on 3/6/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CollegePadsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
