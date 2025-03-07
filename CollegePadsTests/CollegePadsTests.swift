acf722554cbe0f6e66e981f717677aa46f4e24fb
chasepvazquez <Chasepvazquez@icloud.com>
2025-03-06 21:37:01 -0600

    Initial Commit

---
+++ CollegePadsTests/CollegePadsTests.swift
@@ -0,0 +1,16 @@
+//
+//  CollegePadsTests.swift
+//  CollegePadsTests
+//
+//  Created by Chase Vazquez on 3/6/25.
+//
+
+import Testing
+
+struct CollegePadsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
